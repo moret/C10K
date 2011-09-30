523967349d9726b2a6d5da764526210724193d6c
Danilo Moret <github@moret.pro.br>
2011-09-30 12:16:53 -0300

    naive access to twitter api

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
+import json
 import tornado.ioloop
 import tornado.web
+import tornado.httpclient
+
+search = 'http://search.twitter.com/search.json?q=pythonbrasil&result_type=mixed&count=1'
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        self.write("Hello blocking Twitter!\n")
+        http_client = tornado.httpclient.HTTPClient()
+        response = http_client.fetch(search)
+        last_tweet = json.loads(response.body)['results'][0]['text']
+        self.write(last_tweet)
 
 application = tornado.web.Application([
     (r"/", MainHandler),
